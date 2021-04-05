c7a711715039af3bcc4b8fc0838620b8cf13b5e6
Esther Perez Serrajordi <estherperezserrajordi@MacBook-Pro-de-Esther.local>
2021-04-05 19:17:17 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Main management of game states
+
+// Defines the game state
+let game;
+// Diferent game screens
+let splashScreen;
+let gameScreen;
+let bossScreen
+let gameOverScreen;
+let winScreen
